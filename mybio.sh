e6723483ceeaa5ee99463ca6cf6508ad69e0ec8c
Tamal Das <49529498+tamaldas381@users.noreply.github.com>
2020-04-17 13:52:46 -0400

    Create mybio.sh

---
+++ mybio.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Author : TAMAL DEVELOPER
+# Copyright (c) tamaldas.website2.me
+# Script follows here:
+
+echo "What is your Name?"
+read PERSON
+echo "what is your Gender?"
+read GENDER
+echo "what is your Age?"
+read AGE
+echo "What is your Date of Birth?"
+read DATEOFBIRTH
+echo "What is your Address?"
+read ADDRESS
+echo "what is your website Name?"
+read WEBSITENAME
+
+
+echo " YOUR INFORMATION HERE"
+
+echo " Your Name:-$PERSON"
+echo " Your Gender:-$GENDER"
+echo " Your Age:-$AGE"
+echo " Your Date Of Birth:-$DATEOFBIRTH"
+echo " Your Address:-$ADDRESS"
+echo " Your Website Name:-$WEBSITENAME"
